88b7ceb0fe1e95911ca31bb634a04f280ccfe7ea
Sunnexxxx <artykovhondamir@gmail.com>
2023-11-18 22:21:57 +0300

    qweweqe

--- lesson13/lesson12/wsgi.py
+++ lesson13/lesson12/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lesson12 project.
+WSGI config for lesson13 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lesson12.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lesson13.settings')
 
 application = get_wsgi_application()
